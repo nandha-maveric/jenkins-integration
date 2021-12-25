697aa7ee4a2ede95ba68ee117b50d27225ba3e34
Nandha Kumar Shanmugam <nandhaks@maveric-systems.com>
2021-12-25 08:08:03 +0530

    jebkins integration code initial commit

---
+++ src/main/java/com/maveric/poc/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.maveric.poc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @RequestMapping("/welcome")
+    String welcomeUser() {
+        int sonarlintbugCheckUnusedvar = 10;
+        System.out.println("Sonarlint bug check syso.");
+        return "Hello World - v3!";
+    }
+}
---
+++ src/test/java/com/maveric/poc/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maveric.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
